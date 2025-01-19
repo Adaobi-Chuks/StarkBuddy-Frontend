3c2d561b7ce01b9d41d4d68eb65184d7f2e1a9e2
Adaobi-Chuks <chuksaginamada@gmail.com>
2025-01-19 07:48:58 +0100

    Feat: Made navbar more centered on mobile and reduced width of homepage on mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { FaTwitter, FaTelegram } from "react-icons/fa";
 const Navbar = () => {
     return (
         <nav className="flex flex-wrap items-center justify-between px-4 py-4 md:px-6 md:py-6">
+            {/* Logo and Text */}
             <div className="flex items-center">
                 <img
                     src="/assets/StarkBuddyNoBG.png"
@@ -12,25 +13,27 @@ const Navbar = () => {
                 />
                 <span className="text-lg md:text-2xl font-bold text-[#3fe8ab]">StarkBuddy</span>
             </div>
-            <ul className="flex flex-wrap justify-center md:justify-end space-x-4 md:space-x-8 mt-4 md:mt-0">
-                <li>
-                    <a
-                        href="https://x.com/StarkBuddy_ai"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="flex items-center px-3 py-2 md:px-4 md:py-2 bg-[#3fe8ab] text-white rounded-lg hover:bg-[#32d097] transform transition-all"
-                    >
-                        <FaTwitter className="text-xl md:text-2xl" />
-                    </a>
-                </li>
+            
+            {/* Icons */}
+            <ul className="flex items-center space-x-4 md:space-x-6">
                 <li>
                     <a
                         href="https://t.me/starkBuddy_ai_bot"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="flex items-center px-3 py-2 md:px-4 md:py-2 bg-[#3fe8ab] text-white rounded-lg hover:bg-[#32d097] transform transition-all"
+                        className="flex items-center justify-center h-10 w-10 md:h-12 md:w-12 bg-[#3fe8ab] text-white rounded-lg hover:bg-[#32d097] transition-all"
                     >
-                        <FaTelegram className="text-xl md:text-2xl" />
+                        <FaTelegram className="text-xl md:text-2xl align-middle" />
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="https://x.com/StarkBuddy_ai"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="flex items-center justify-center h-10 w-10 md:h-12 md:w-12 bg-[#3fe8ab] text-white rounded-lg hover:bg-[#32d097] transition-all"
+                    >
+                        <FaTwitter className="text-xl md:text-2xl align-middle" />
                     </a>
                 </li>
             </ul>
