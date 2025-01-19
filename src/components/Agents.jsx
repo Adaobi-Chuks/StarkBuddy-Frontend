450cfd6be93b3164e82b92c8ce052388fbca5e6d
Adaobi-Chuks <chuksaginamada@gmail.com>
2025-01-19 07:32:18 +0100

    Feat: Add mobile responsiveness to homepage

--- src/components/Agents.jsx
+++ src/components/Agents.jsx
@@ -3,31 +3,25 @@ import { FaTwitter, FaTelegram } from "react-icons/fa";
 
 const Agent = () => {
     return (
-        <div className="flex justify-center items-center h-full py-8 mb-14">
-            <ul className="flex space-x-20">
-                <li>
-                    <a
-                        href="https://x.com/StarkBuddy_ai"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="flex items-center px-6 py-3 bg-[#3fe8ab] text-white text-2xl rounded-lg space-x-3 hover:bg-[#32d097] transform hover:scale-110 transition-all"
-                    >
-                        <FaTwitter className="text-4xl" />
-                        <span>Twitter Agent</span>
-                    </a>
-                </li>
-                <li>
-                    <a
-                        href="https://t.me/starkBuddy_ai_bot"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="flex items-center px-6 py-3 bg-[#3fe8ab] text-white text-2xl rounded-lg space-x-3 hover:bg-[#32d097] transform hover:scale-110 transition-all"
-                    >
-                        <FaTelegram className="text-4xl" />
-                        <span>Telegram Agent</span>
-                    </a>
-                </li>
-            </ul>
+        <div className="flex flex-col md:flex-row justify-center items-center space-y-6 md:space-y-0 md:space-x-8 py-8">
+            <a
+                href="https://x.com/StarkBuddy_ai"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center justify-center w-[240px] px-6 py-3 bg-[#3fe8ab] text-white text-lg md:text-2xl rounded-lg space-x-3 hover:bg-[#32d097] transform hover:scale-105 transition-all"
+            >
+                <FaTwitter className="text-2xl md:text-4xl" />
+                <span>Twitter Agent</span>
+            </a>
+            <a
+                href="https://t.me/starkBuddy_ai_bot"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center justify-center w-[240px] px-6 py-3 bg-[#3fe8ab] text-white text-lg md:text-2xl rounded-lg space-x-3 hover:bg-[#32d097] transform hover:scale-105 transition-all"
+            >
+                <FaTelegram className="text-2xl md:text-4xl" />
+                <span>Telegram Agent</span>
+            </a>
         </div>
     );
 };
