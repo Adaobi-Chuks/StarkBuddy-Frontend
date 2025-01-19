d80fac7f750d567b3e5db2375f4725b740dfea34
Adaobi-Chuks <chuksaginamada@gmail.com>
2025-01-19 09:18:07 +0100

    Chore: Add proper formatting

--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -2,22 +2,21 @@ import React from "react";
 
 const ChatInput = ({ value, onChange, onKeyPress, onSend }) => (
     <div className="flex items-center space-x-2 mt-4 relative w-full">
-      <input
-        type="text"
-        placeholder="Type a message..."
-        value={value}
-        onChange={onChange}
-        onKeyPress={onKeyPress}
-        className="flex-1 px-4 py-2 bg-[#333] text-white rounded-lg focus:outline-none cursor-text z-10"
-      />
-      <button
-        onClick={onSend}
-        className="px-4 py-2 bg-[#3fe8ab] text-black font-bold rounded-lg hover:bg-[#1c7e67] transition z-20 flex items-center justify-center"
-      >
-        Send
-      </button>
+        <input
+            type="text"
+            placeholder="Type a message..."
+            value={value}
+            onChange={onChange}
+            onKeyPress={onKeyPress}
+            className="flex-1 px-4 py-2 bg-[#333] text-white rounded-lg focus:outline-none cursor-text z-10"
+        />
+        <button
+            onClick={onSend}
+            className="px-4 py-2 bg-[#3fe8ab] text-black font-bold rounded-lg hover:bg-[#1c7e67] transition z-20 flex items-center justify-center"
+        >
+            Send
+        </button>
     </div>
-  );
-  
-  export default ChatInput;
-  
+);
+
+export default ChatInput;
